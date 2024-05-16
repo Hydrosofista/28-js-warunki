8ac3f786cd8b4a159afdc87ca8664c92e655b6f1
Hydrosofista <hydrosofista@gmail.com>
2024-05-16 14:21:54 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let number = 12;
+
+if (number > 10) {
+    console.log('Podana liczba jest większa od 10')
+} else if (number < 10 ) {
+    console.log('Podana liczba jest mniejsza od 10')
+} else if (number === 10 ) {
+    console.log('Podana liczba to 10')
+} else {
+    console.log('Nastąpił błąd')
+}
